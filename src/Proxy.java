88a083ed25593c87a4ec65ee09c43c2d02b01bdf
Miguel Azevedo Lopes <lopesazevedom@gmail.com>
2022-10-11 19:25:32 +0100

    just empty classes

---
+++ src/Proxy.java
@@ -0,0 +1,9 @@
+import java.util.Map;
+
+public class Proxy {
+    Map <String,Topic> topics;
+
+    public Message getSubscriberMessage(String subscriberID,String topic){
+        return null;
+    }
+}
