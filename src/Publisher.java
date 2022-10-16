b8ab8baf7c094f98ef3a1be37795f12653383dfb
Mariana Monteiro <up202003480@fe.up.pt>
2022-10-16 16:57:48 +0100

    Started get and put functions

--- src/Publisher.java
+++ src/Publisher.java
@@ -1,3 +1,29 @@
+import org.zeromq.ZMQ.Socket;
+import org.zeromq.*;
+import org.zeromq.ZContext;
+import java.sql.Timestamp;
+import org.zeromq.ZMsg;
 public class Publisher {
-    
+    public final String PUB_SOCKET="5555";
+
+    public Publisher(ZContext ctx,String id){
+        super(ctx,id);
+    }
+    public void put (String topic, String message){
+        String args[] = new String[2];
+        args[0]=topic;
+        args[1]=message;
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        Socket pubSocket = ctx.createSocket(SocketType.REQ);
+
+        pubSocket.connect("tcp://*:" + PUB_SOCKET);
+
+        Message msg = new Message(this.id,"PUT",timestamp,args);
+        String message=msg.createMessage();
+
+        pubSocket.send(message);
+
+    }
+
+
 }
--- src/Subscriber.java
+++ src/Subscriber.java
@@ -1,3 +1,21 @@
 public class Subscriber {
-    
+
+    public final String SUB_SOCKET="5556";
+
+    public Subscriber(ZContext ctx,String id){
+        super(ctx,id);
+    }
+    public void get(String topic){
+        String args= new String[1];
+        args[0]=topic;
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        Socket subSocket = ctx.createSocket(SocketType.REQ);
+
+        subSocket.connect("tcp://*:" + SUB_SOCKET);
+
+        ZMsg msg = new Message(this.id,"GET",timestamp,args);
+        String message=msg.createMessage();
+
+        subSocket.send(message);
+    }
 }
